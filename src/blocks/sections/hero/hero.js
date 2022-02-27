6d24105c84cd4c2d354bedb8664a8ca6225387ea
galustoff <galustoff.dan@yandex.ru>
2022-02-27 12:53:44 +0300

    hero

---
+++ src/blocks/sections/hero/hero.js
@@ -0,0 +1,7 @@
+import { SectionCreator } from "../../../components/SectionCreator/SectionCreator.js";
+import html from "./hero.html";
+import "./hero.sass";
+
+const hero = new SectionCreator(html);
+
+export default hero;
