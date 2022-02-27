5e343c092c83cad0d0486398d5337ba6907817b3
galustoff <galustoff.dan@yandex.ru>
2022-02-27 13:06:21 +0300

    pipercoin

---
+++ src/blocks/sections/pipercoin/pipercoin.js
@@ -0,0 +1,7 @@
+import { SectionCreator } from "../../../components/SectionCreator/SectionCreator.js";
+import html from "./pipercoin.html";
+import "./pipercoin.sass";
+
+const pipercoin = new SectionCreator(html);
+
+export default pipercoin;
--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import footer from "./blocks/footer/footer.js";
 import jumbotron from "./blocks/sections/jumbotron/jumbotron.js";
 import problem from "./blocks/sections/problem/problem.js";
 import hero from "./blocks/sections/hero/hero.js";
+import pipercoin from "./blocks/sections/pipercoin/pipercoin.js";
 
 header.render();
 footer.render();
 jumbotron.render();
 problem.render();
 hero.render();
+pipercoin.render();
