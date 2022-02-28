22b5a57a9ef7b48e19ae6a9be7b103a08e786828
galustoff <galustoff.dan@yandex.ru>
2022-02-28 21:53:18 +0300

    build for publication

--- src/utils/constants.js
+++
@@ -1,7 +0,0 @@
-export const teamConfig = {
-    cardsContainerSel: '.team__cards',
-    cardTemplateSel: '#team__card',
-    cardPhotoSel: '.team__photo',
-    cardNameSel: '.team__name',
-    cardPositionSel: '.team__position',
-};
